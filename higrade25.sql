45e747e5699c25e9baa57d3b96dd9d9f291074ac
Punitha <punitha07ed@gmail.com>
2025-08-12 12:07:53 +0530

    db commiting

--- higrade25.sql
+++ higrade25.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2025 at 02:31 PM
+-- Generation Time: Aug 12, 2025 at 08:36 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,10 +43,67 @@ CREATE TABLE `contact_form` (
 --
 
 INSERT INTO `contact_form` (`id`, `firstname`, `lastname`, `email`, `phone`, `address`, `message`, `submitted_at`) VALUES
-(1, 'punitha', 'test', 'punithatha07@gmail.com', '89798798798798', 'Sample address', 'Test message', '2025-07-21 04:54:16'),
-(8, 'punitha', 'test', 'jesuspunitha22@gmail.com', '9798789789', 'sample address', 'sample msg', '2025-07-21 07:25:43'),
-(10, 'punitha', 'test', 'punithatha45@gmail.com', '97979797998', 'sample add', 'sample msg', '2025-07-21 08:45:48'),
-(11, 'test', 'test', 'test435@gmail.com', '897897987', 'test', 'test', '2025-07-21 08:50:36');
+(56, 'Punitha', 'test', 'punitha22@gmail.com', '89798797979', 'sample address', 'sample messages', '2025-08-12 04:19:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `home_form`
+--
+
+CREATE TABLE `home_form` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `home_form`
+--
+
+INSERT INTO `home_form` (`id`, `name`, `email`, `phone`, `message`, `created_at`) VALUES
+(11, NULL, 'jesuspunitha22@gmail.com', '9879879798798', 'sample messages', '2025-08-12 04:37:09'),
+(12, 'Punitha', 'punitha44@gmail.com', '989979878979879', 'sample emssages', '2025-08-12 04:39:32'),
+(13, 'Punitha', 'punitha441@gmail.com', '89789789798', 'sample messages', '2025-08-12 04:41:12'),
+(14, 'testt', 'est54@gmail.com', '080989080938', 'smple message', '2025-08-12 05:26:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `newsletter`
+--
+
+CREATE TABLE `newsletter` (
+  `id` int(11) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `subscribed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `newsletter`
+--
+
+INSERT INTO `newsletter` (`id`, `email`, `subscribed_at`) VALUES
+(1, 'oybt4389@gmail.com', '2025-08-12 04:51:23'),
+(2, 'testou43@gamil.com', '2025-08-12 04:52:35'),
+(3, 'tes4645@gmail.com', '2025-08-12 04:52:47'),
+(4, 'punitha43@gmail.com', '2025-08-12 04:55:54'),
+(5, 'punitha4334@gmail.com', '2025-08-12 04:56:08'),
+(6, 'puntha46@gmail.com', '2025-08-12 04:56:59'),
+(7, 'yrr567@gmail.com', '2025-08-12 04:58:03'),
+(8, 'tesssg456@gmail.com', '2025-08-12 04:58:43'),
+(9, '43534@gmail.com', '2025-08-12 05:01:21'),
+(10, 'tre546@gmail.com', '2025-08-12 05:02:54'),
+(11, 'ytr768@gmail.com', '2025-08-12 05:03:31'),
+(12, 'test4365@gmail.com', '2025-08-12 05:04:33'),
+(13, 'testou354@gmail.com', '2025-08-12 05:05:26'),
+(14, 'test67@gmail.com', '2025-08-12 05:06:01'),
+(15, 'terrt@gamil.com', '2025-08-12 05:10:25'),
+(16, 'test54@gmail.com', '2025-08-12 05:26:45'),
+(17, 'test@gmail.com', '2025-08-12 05:46:39');
 
 --
 -- Indexes for dumped tables
@@ -60,6 +117,19 @@ ALTER TABLE `contact_form`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `home_form`
+--
+ALTER TABLE `home_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `newsletter`
+--
+ALTER TABLE `newsletter`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -67,7 +137,19 @@ ALTER TABLE `contact_form`
 -- AUTO_INCREMENT for table `contact_form`
 --
 ALTER TABLE `contact_form`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT for table `home_form`
+--
+ALTER TABLE `home_form`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `newsletter`
+--
+ALTER TABLE `newsletter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
